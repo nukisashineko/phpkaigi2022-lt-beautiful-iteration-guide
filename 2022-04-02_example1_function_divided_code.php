48852545bcef218be4a76a3381738f8817ad994c
nukisashineko <nukisashineko@users.noreply.github.com>
2022-04-10 08:53:55 +0900

    fix main function

--- 2022-04-02_example1_function_divided_code.php
+++ 2022-04-02_example1_function_divided_code.php
@@ -18,6 +18,28 @@ class Example1 {
         }
         return $list2;
     }
+
+    private static function array_filter__is_even(array $list2){
+        $even_list = [];
+        foreach($list2 as $x){
+            if(self::is_even($x)){
+                $even_list []= $x;
+            }
+        }
+        return $even_list;
+    }
+
+    private static function array_filter__is_odd(array $list2){
+        $odd_list = [];
+        foreach($list2 as $x){
+            if(self::is_odd($x)){
+                $odd_list []= $x;
+            }
+        }
+        return $odd_list;
+    }
+
+
     private static function array_sum(array $list){
         $sum = 0;
         foreach($list as $x){
@@ -26,6 +48,20 @@ class Example1 {
         return $sum;
     }
 
+    public static function main(){
+        $list = [1,2,3,4,5,6,7,8,9,10];
+
+        // 奇数の場合は2乗した値に置き換えて！
+        $list2 = self::array_map__is_odd_then_power_2($list);
+
+        // 偶数・奇数で配列を分けてほしい！
+        $odd_list = self::array_filter__is_odd($list2);
+        $even_list = self::array_filter__is_even($list2);
+
+        // 分けた配列の各小計を出してほしい！
+        $odd_sum = self::array_sum($odd_list);
+        $even_sum = self::array_sum($even_list);
+    }
 }
 
 Example1::main();
