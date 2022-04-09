02d2975ac710a7e90a60f8cdcaaaef33975003a1
nukisashineko <nukisashineko@users.noreply.github.com>
2022-04-10 08:54:47 +0900

    add return type

--- 2022-04-02_example1_function_divided_code.php
+++ 2022-04-02_example1_function_divided_code.php
@@ -1,55 +1,61 @@
 <?php
 
-class Example1 {
+class Example1
+{
     private static function is_odd(int $x): bool
     {
         return $x % 2 === 1;
     }
 
-    private  static function is_even(int $x): bool
+    private static function is_even(int $x): bool
     {
         return $x % 2 === 0;
     }
 
-    private static function array_map__is_odd_then_power_2(array $list): array{
+    private static function array_map__is_odd_then_power_2(array $list): array
+    {
         $list2 = [];
-        foreach ($list as $x){
-            $list2[] = self::is_odd($x)? $x ** 2: $x;
+        foreach ($list as $x) {
+            $list2[] = self::is_odd($x) ? $x ** 2 : $x;
         }
         return $list2;
     }
 
-    private static function array_filter__is_even(array $list2){
+    private static function array_filter__is_even(array $list2): array
+    {
         $even_list = [];
-        foreach($list2 as $x){
-            if(self::is_even($x)){
-                $even_list []= $x;
+        foreach ($list2 as $x) {
+            if (self::is_even($x)) {
+                $even_list [] = $x;
             }
         }
         return $even_list;
     }
 
-    private static function array_filter__is_odd(array $list2){
+    private static function array_filter__is_odd(array $list2): array
+    {
         $odd_list = [];
-        foreach($list2 as $x){
-            if(self::is_odd($x)){
-                $odd_list []= $x;
+        foreach ($list2 as $x) {
+            if (self::is_odd($x)) {
+                $odd_list [] = $x;
             }
         }
         return $odd_list;
     }
 
 
-    private static function array_sum(array $list){
+    private static function array_sum(array $list): array
+    {
         $sum = 0;
-        foreach($list as $x){
+        foreach ($list as $x) {
             $sum += $x;
         }
         return $sum;
     }
 
-    public static function main(){
-        $list = [1,2,3,4,5,6,7,8,9,10];
+    public static function main()
+    {
+        $list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
         // 奇数の場合は2乗した値に置き換えて！
         $list2 = self::array_map__is_odd_then_power_2($list);
@@ -61,6 +67,8 @@ class Example1 {
         // 分けた配列の各小計を出してほしい！
         $odd_sum = self::array_sum($odd_list);
         $even_sum = self::array_sum($even_list);
+
+        $a = '';
     }
 }
 
