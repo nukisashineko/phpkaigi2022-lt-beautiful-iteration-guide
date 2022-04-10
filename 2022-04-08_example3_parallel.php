d4bfab03498e2a6769f419ea35e96d6451381dd9
nukisashineko <nukisashineko@users.noreply.github.com>
2022-04-10 10:07:19 +0900

    Update 2022-04-08_example3_parallel.php

--- 2022-04-08_example3_parallel.php
+++ 2022-04-08_example3_parallel.php
@@ -1,27 +1,30 @@
 <?php
 
+
 class Example3
 {
-    private static function identity_async(int $x): \GuzzleHttp\Promise
+    private static function identity_async(int $x): \GuzzleHttp\Promise\PromiseInterface
+    {
         $client = new \GuzzleHttp\Client();
-        return $client->requestAsync('GET', "http://example.com/identity?x={$x}");;
+        return $client->requestAsync('GET', "http://example.com/identity?x={$x}");
     }
 
-    private static function normal_processing(array $list): array {
+    private static function normal_processing(array $list): array
+    {
         $responses = [];
-        foreach($list as $x){
+        foreach ($list as $x) {
             $promise = self::identity_async($x);
-            $responses[] = \GuzzleHttp\Promise\all([$promise])->wait();
+            $responses[] = \GuzzleHttp\Promise\all([$promise])->wait()[0];
         }
-
         $result = [];
         foreach ($responses as $response) {
-            $result[] = (int)$response->getBody()->getContents();
+            $result[] = (int)json_decode($response->getBody()->getContents(), true)['result'];
         }
         return $result;
     }
 
-    private static function parallel_processing(array $list): array {
+    private static function parallel_processing(array $list): array
+    {
         $promises = array_map([self::class, 'identity_async'], $list);
         $responses = \GuzzleHttp\Promise\all($promises)->wait();
 
@@ -32,19 +35,20 @@ class Example3
         return $result;
     }
 
-    private static function lazy__parallel_processing(array $list): Generator{
+    private static function lazy__parallel_processing(array $list): Generator
+    {
         $promises = array_map([self::class, 'identity_async'], $list);
         $responses = \GuzzleHttp\Promise\all($promises)->wait();
         foreach ($responses as $response) {
-            yield (int)$response->getBody()->getContents();
+            yield (int)json_decode($response->getBody()->getContents(), true)['result'];
         }
     }
 
     public static function main()
     {
-        $array1 = self::normal_processing( [1, 2, 3]);
-        $array2 = self::parallel_processing( [1, 2, 3]);
-        $array3 = iterator_to_array(self::lazy__parallel_processing( [1, 2, 3]));
+        $array1 = self::normal_processing([1, 2, 3]);
+        $array2 = self::parallel_processing([1, 2, 3]);
+        $array3 = iterator_to_array(self::lazy__parallel_processing([1, 2, 3]));
         var_dump($array1 === $array2); # => true
         var_dump($array2 === $array3); # => true
     }
